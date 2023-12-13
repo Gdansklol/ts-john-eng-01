e5c21a565cb3c773cbe5a622c63b01ca2290649d
swedenebomi <swedenebomi@gamil.com>
2023-12-13 10:44:44 +0100

    add checkmark btn

--- src/components/Chapter1.tsx
+++ src/components/Chapter1.tsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 const Chapter1: React.FC = () => {
+  const [memorized, setMemorized] = useState(false);
+
+  const handleMemorizeClick = () => {
+    setMemorized(!memorized);
+  };
+
+
   return (
     <div>
       <h1>Christ, the Eternal Word(1)</h1>
@@ -13,6 +20,9 @@ const Chapter1: React.FC = () => {
       <p>John 1:2</p>
       <p>He was with God in the beginning.</p>
       <br/>
+      <button onClick={handleMemorizeClick}>
+        {memorized ? 'Recitation completed !' : 'recitation checkmark'}
+      </button>
       <h3>Understand by chunk!</h3>
       <h3>덩어리별로 이해하세요!</h3>
       {/* Add code for displaying and memorizing verses */}
